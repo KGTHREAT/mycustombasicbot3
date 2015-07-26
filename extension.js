f905916f37c28b540b87c5c7b64b987fe031b0ef
KGTHREAT <gualtieri.michael@gmail.com>
2015-07-26 16:51:17 -0700

    Added !submissions. Added lockskip reason. Added !chillnation command.

--- extension.js
+++ extension.js
@@ -98,6 +98,32 @@
             }
         };
         
+        bot.commands.submissionsCommand = {
+            command: 'submissions',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Submit your music here: http://www.alltrapnation.com/trap-submission/ /");
+                }
+            }
+        };
+        
+        bot.commands.ChillnationCommand = {
+            command: 'submissions',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Subscribe to Chill Nation: https://www.youtube.com/user/ChillStepNation /");
+                }
+            }
+        };
+        
         bot.commands.changelog = {
             command: 'changelog',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'bouncer', //Minimum user permission to use the command
@@ -147,12 +173,13 @@
             ["unavailable", "The song you played was not available for some users. "],
             ["na", "The song you played was not available for some users. "],
             ["troll", "The song you played is considered trolling if you play it again you will be banned"]
+            ["crap", "Bruh. your song is complete Crap. Smh."]
         ],
         afkpositionCheck: 15,
         afkRankCheck: "ambassador",
         motdEnabled: true,
         motdInterval: 10,
-        motd: "We Deliver Fire.",
+        motd: "We Deliver that phat bass.",
         filterChat: true,
         etaRestriction: false,
         welcome: true,
