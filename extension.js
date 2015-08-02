bd5e9ef2c64a1b7df40d35b8a1349bbb80f8206d
aalinnfs <aalinnfs@gmail.com>
2015-08-02 23:58:42 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -30,7 +30,20 @@
                 }
             }
         };
-
+        
+        bot.commands.autofapCommand = {
+            command: 'autofap',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("http://i.imgur.com/YBlC5cW.gif");
+                }
+            }
+        };
+        
         bot.commands.clearqueueCommand = {
             command: 'clearqueue',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'manager', //Minimum user permission to use the command
