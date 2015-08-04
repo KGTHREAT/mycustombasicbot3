90dc6ab95440a83046d2a5c2895ec5ec41570cb9
KGTHREAT <gualtieri.michael@gmail.com>
2015-08-04 14:36:22 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -124,6 +124,19 @@
             }
         };
         
+        bot.commands.badgeCommand = {
+            command: 'badge',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Have RCS? Want to stand out from the rest? Submit your very own plug.dj badge here: http://bit.ly/CustomBadge to be applied to the room.");
+                }
+            }
+        };
+        
         bot.commands.submissionsCommand = {
             command: 'submissions',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
